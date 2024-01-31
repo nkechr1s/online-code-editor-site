df37e7988a876974118e78b02a0d8bb60ad3660e
Nikos Kechris <nkechris22@gmail.com>
2024-01-31 15:27:12 +0200

    Fix ts

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Link, LinkProps } from "react-router-dom";
 import { ButtonProps } from "src/lib/types";
 import styles from "./Button.module.css";
 
@@ -7,14 +9,29 @@ const buttonClassNames = {
 };
 
 const Button = ({ variant, text, onClick, href, ...props }: ButtonProps) => {
-  const ButtonComponent = href ? "a" : "button";
-  const buttonProps = {
-    ...(href ? { href } : { onClick }),
-    ...props,
-    className: buttonClassNames?.[variant] ?? buttonClassNames.primary,
-  };
+  const ButtonComponent = href ? Link : "button";
 
-  return <ButtonComponent {...buttonProps}>{text}</ButtonComponent>;
+  let buttonProps:
+    | React.AnchorHTMLAttributes<HTMLAnchorElement>
+    | React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+  if (href) {
+    buttonProps = {
+      to: href,
+      ...props,
+      className: buttonClassNames?.[variant] ?? buttonClassNames.primary,
+    } as LinkProps;
+  } else {
+    buttonProps = {
+      onClick,
+      ...props,
+      type: "button",
+      className: buttonClassNames?.[variant] ?? buttonClassNames.primary,
+    };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return <ButtonComponent {...(buttonProps as any)}>{text}</ButtonComponent>;
 };
 
 export default Button;
