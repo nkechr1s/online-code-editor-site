3fd23438476282bdb8d3db4125b16b8fda173444
Nikos Kechris <nkechris22@gmail.com>
2024-01-31 21:16:21 +0200

    Comm

--- src/shell.tsx
+++ src/shell.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import { router } from "src/routes";
 import { Header, Footer } from "src/containers";
 import { GlobalProvider } from "src/context/GlobalContext";
 
