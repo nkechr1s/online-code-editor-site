79703de47024b6e3ca1e290ffeae47f401822b86
Nikos Kechris <nkechris22@gmail.com>
2024-02-01 20:50:43 +0200

    Fix

--- src/components/Cards/CardsGrid/CardsGrid.tsx
+++ src/components/Cards/CardsGrid/CardsGrid.tsx
@@ -4,20 +4,16 @@ import styles from "./CardsGrid.module.css";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CardsGrid = ({ cardsData }: any) => {
   return (
-    <div className={styles.gridContainer}>
-      {Array.isArray(cardsData) &&
-        cardsData.map((card, index) => (
-          <Card
-            key={index + card.id}
-            id={card.id}
-            title={card.title}
-            subTitle={card.subTitle}
-            description={card.description}
-            imgUrl={card.imgUrl}
-            imgAlt={card.imgAlt}
-          />
-        ))}
-    </div>
+    <>
+      {cardsData && (
+        <div className={styles.gridContainer}>
+          {Array.isArray(cardsData) &&
+            cardsData.map((card, index) => (
+              <Card key={index + card.id} {...card} />
+            ))}
+        </div>
+      )}
+    </>
   );
 };
 
